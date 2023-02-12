4fa3e166b369ec754967668401ddeb43eb011470
Patience Hodzi <patiencehodzi@gmail.com>
2023-02-12 11:36:09 +0000

    filter-function

--- print.js
+++ print.js
@@ -1,19 +1,19 @@
-for ( let i = 1; i <= 100; i++ )
-{
-  if ( i%5 === 0  &&  i%7 === 0)
-  {
-    console.log("FastTrack" );
-  }
-  else if ( i%7 === 0 ) 
-  {
-    console.log("Track" );
-  }
-  else if ( i%5 === 0 ) 
-  {
-    console.log( "Fast" );
-  }
-  else
-  {
-    console.log(i);
-  }
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 === 0 && i % 7 === 0) {
+//     console.log("FastTrack");
+//   } else if (i % 7 === 0) {
+//     console.log("Track");
+//   } else if (i % 5 === 0) {
+//     console.log("Fast");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+function filter() {
+  let numArray = [14, 56, 20, 60, 87, 37];
+  const filteredArray = numArray.filter((num) => {
+    return num > 50;
+  });
+  console.log(filteredArray);
 }
